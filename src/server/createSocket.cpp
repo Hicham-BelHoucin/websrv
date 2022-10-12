01512ce1447edf008c80a857e65c2406bf0f6b59
Ousama Beaj <obeaj@e2r3p1.1337.ma>
2022-10-12 17:46:05 +0100

    micro webserv

---
+++ src/server/createSocket.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   createSocket.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obeaj <obeaj@student.1337.ma>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/12 15:58:09 by obeaj             #+#    #+#             */
+/*   Updated: 2022/10/12 16:51:01 by obeaj            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "createSocket.hpp"
+createSocket::createSocket()
+{
+    
+}
+createSocket::createSocket(int domaine, int type, int protocol, u_int32_t ip, int port)
+{
+    if ((sockfd = socket(domaine, type, protocol)) < 0)
+        throw std::runtime_error("Error: Socket cannot be created ! \n");
+    address.sin_family = domaine;
+    address.sin_addr.s_addr = ip;
+    address.sin_port = htons(port);
+    if ()
+}
+createSocket::~createSocket()
+{
+    
+}
