c02fe9cef7c1ecf7e1ba8c6327db103d5374b381
Hicham Bel houcin <hbel-hou@e2r5p11.1337.ma>
2022-10-27 11:15:07 +0100

    create requset

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: hbel-hou <hbel-hou@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/24 10:20:36 by hbel-hou          #+#    #+#              #
-#    Updated: 2022/10/24 10:30:16 by hbel-hou         ###   ########.fr        #
+#    Updated: 2022/10/26 14:32:39 by hbel-hou         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,7 +16,7 @@ CC = c++
 
 # CFLAGS = -Wall -Wextra -Werror -std=c++98
 
-SRCS = src/parsing/parsing.cpp src/request/request.cpp src/response/response.cpp src/server/createSocket.cpp src/server/server.cpp src/webserv.cpp
+SRCS = src/parsing/parsing.cpp src/request/request.cpp src/utils/utils.cpp src/response/response.cpp src/server/createSocket.cpp src/server/server.cpp src/webserv.cpp
 
 SRCOBJ = $(SRCS:.cpp=.o)
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -6,7 +6,7 @@
 /*   By: hbel-hou <hbel-hou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 09:02:50 by obeaj             #+#    #+#             */
-/*   Updated: 2022/10/25 13:50:11 by hbel-hou         ###   ########.fr       */
+/*   Updated: 2022/10/27 11:12:42 by hbel-hou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,21 @@ static std::string readFile(std::string name)
     return text;
 }
 
-server::server() : max_fd(0)
+server::server() : max_fd(FD_SETSIZE)
 {
 	FD_ZERO(&current_sockets);
+}
+
+void	server::create_requset(int connection)
+{
+	char            buff[100000];
+	int            baytesread;
+
+    baytesread = read(connection, buff, 100000);
+	_requset = std::string(buff);
+	system("clear");
+	// std::cout << "requset : " << _requset << std::endl;
+	std::cout << baytesread << std::endl;
 }
 
 void server::setSocket(vector&	s)
@@ -58,8 +70,9 @@ createSocket server::getSocket(int fd)
 void	server::acceptConnection(createSocket _socket)
 {
 	int             connection;
-    char            buff[1024];
-    std::string response;
+    std::string 	response;
+	char            buff[1024];
+
 	response =
         "HTTP/1.1 200 OK\n"
         "Date: Thu, 19 Feb 2009 12:27:04 GMT\n"
@@ -76,7 +89,7 @@ void	server::acceptConnection(createSocket _socket)
     connection = _socket._accept();
     if (connection == -1)
         std::cout << "error accepting connection" << std::endl;
-    read(connection, buff, 1024);
+	create_requset(connection);
     write(connection, response.c_str(), response.size());
     close(connection);
 }
